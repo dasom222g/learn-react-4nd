5d9734c6d3a2811f9317d655350df534ac8b9980
dasom222g <dasom228@gmail.com>
2026-02-25 16:16:14 +0900

    Feat: react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import { useState } from "react";
-import Card from "./components/Card";
-import Counter from "./components/Counter";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Test from "./pages/Test";
+import Chat from "./pages/Chat";
 
 function App() {
-  // logic
-  const [username, setUserName] = useState("후츠릿");
-  const handleClick = (data) => {
-    console.log("부모님! 클릭 됐습니다!!", data);
-    if (data === "후츠릿") {
-      setUserName("히히힛");
-    }
-  };
-
-  // view
   return (
-    <div className="App">
-      <Card username={username} discount={12} onButtonClick={handleClick} />
-      <Card username={"김완수"} discount={15} onButtonClick={handleClick} />
-      <Card username={"김태희"} discount={70} onButtonClick={handleClick} />
-      <Card onButtonClick={handleClick} />
-
-      {/* <Counter /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Test />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
